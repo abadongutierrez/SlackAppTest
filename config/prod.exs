7ae5ac9b42eff4ca7e52cff2ff363526e45c2523
abadon.gutierrez <abadon.gutierrez@gmail.com>
2017-09-17 23:14:26 -0500

    Fixed config

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :slack_test_app, SlackTestAppWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :slack_test_app, SlackTestAppWeb.Repo,
+config :slack_test_app, SlackTestApp.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
